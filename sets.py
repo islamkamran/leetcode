8e93b6ac6da8da243c37352ad34ef6fa7e186611
Muhammad Islam Kamran <islamkamran332@gmail.com>
2024-12-16 23:56:32 +0500

    sets basic functionality practice

---
+++ sets.py
@@ -0,0 +1,13 @@
+# Un ordered sets in C++ are sets in python implemened through hashmap
+
+# unordered_set = {1,2,3,5,7,8,9}
+# unordered_set.add(4)
+# unordered_set.remove(2)
+# unordered_set.discard(9)
+# elem = unordered_set.pop()
+# print(unordered_set)
+# unordered_set.clear()
+# print(elem)
+# print(unordered_set)
+
+
