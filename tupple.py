6f883d2433dbd3c4b24f8b00831e7fa255b67ae8
Muhammad Islam Kamran <islamkamran332@gmail.com>
2024-12-20 01:00:39 +0500

    pair in c++ as tuple in python done

---
+++ tupple.py
@@ -0,0 +1,21 @@
+from collections import namedtuple
+
+# pair = (1,"hello")
+
+# print(pair[0])
+# print(pair[1])
+
+# first,second = pair
+
+# print(first)
+# print(second)
+
+# named_pair = namedtuple('pair',['first','second'])
+
+# p = named_pair('2','islam')
+
+# print(p.first)
+# print(p.second)
+
+arr_pair = ((1,2),("islam","kamran"))
+print(arr_pair[1][0])
